1bc200ea7608b2a2d2fb9783f506193e11956226
bbarnefiher <bb222@pvsd.org>
2018-10-15 11:22:08 -0400

    end of class

--- src/com/company/encryption.java
+++ src/com/company/encryption.java
@@ -4,12 +4,22 @@ package com.company;
  * Created by bb222 on 10/15/18.
  */
 public class encryption {
-    String userPhrase = "";
+    String userPhrase = "";\
+    
     public encryption(String x){
         userPhrase = x;
     }
+    int[] userPhraseArray = new int[userPhrase.length()];
     private String encryptMethod(){
-        if ()
+        for(int i = 0; i <=userPhrase.length(); i++){
+            char currentChar = userPhrase.charAt(i);
+            int ASCIIchar = (int) currentChar;
+            userPhraseArray[i] = ASCIIchar;
+        }
+
+
+
+
     }
 
 }
