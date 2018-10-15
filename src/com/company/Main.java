2b37e88ebc568c8fae8e7fe0812d8642a08a69d8
bbarnefiher <bb222@pvsd.org>
2018-10-15 11:05:25 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner stringReader = new Scanner(System.in);
+        encryption userInput = new encryption(stringReader.nextLine());
+
+
+
+
+
+
+    }
+}
---
+++ src/com/company/encryption.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by bb222 on 10/15/18.
+ */
+public class encryption {
+    String userPhrase = "";
+    public encryption(String x){
+        userPhrase = x;
+    }
+    private String encryptMethod(){
+        if ()
+    }
+
+}
