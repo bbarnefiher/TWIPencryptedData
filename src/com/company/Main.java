383fba3806b0a89c82eac003fa738da1cd315751
BuildTools <unconfigured@null.spigotmc.org>
2018-10-18 19:20:45 -0400

    i did it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,40 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner stringReader = new Scanner(System.in);
-        encryption userInput = new encryption(stringReader.nextLine());
+        Scanner stringReaderForEncryption = new Scanner(System.in);
+        Scanner stringReaderForDecryption = new Scanner(System.in);
+        Scanner stringReaderForDecryptionTwo = new Scanner(System.in);
+        Scanner intReader = new Scanner (System.in);
+        int userLoop = -1;
+        while (true) {
+
+            System.out.println("\nEnter 0 to exit, 1 to encrypt a phrase, or 2 to decrypt a phrase.");
+            userLoop = intReader.nextInt();
+
+
+            if (userLoop == 1){
+                System.out.println("Enter the phrase you want encrypted.");
+                encryption userInput = new encryption(stringReaderForEncryption.nextLine());
+
+                System.out.println(userInput.getEncryptedPhrase());
+            }
+            else if (userLoop == 2){
+                encryption userDecrypt = new encryption(stringReaderForDecryption.nextLine());
+                System.out.println("Enter the phrase you want decrypted.");
+                String encryptedPhrase = stringReaderForDecryptionTwo.next();
+                System.out.println("Enter the key to decrypt your phrase.");
+                int userKey = intReader.nextInt();
+
+                System.out.println(userDecrypt.getDecryptedPhrase(encryptedPhrase, userKey));
+            }
+            else {
+                break;
+            }
 
 
 
-
-
-
+        }
+        System.out.println("cya");
+        System.exit(0);
     }
 }
--- src/com/company/encryption.java
+++ src/com/company/encryption.java
@@ -4,22 +4,60 @@ package com.company;
  * Created by bb222 on 10/15/18.
  */
 public class encryption {
-    String userPhrase = "";\
-    
+    String userPhrase = "";
+    String userPhraseEncrypted = "";
+    int decryptKey = -1;
+
+
+
     public encryption(String x){
         userPhrase = x;
+
+
+        decryptKey = (int) (Math.random() * 10000);
+        while ((decryptKey % 10 == 0)) {
+            decryptKey = (int) (Math.random() * 10000);
+        }
+        //forces key to not be something that would not change the encrypted message to decrypted
     }
-    int[] userPhraseArray = new int[userPhrase.length()];
+
+
     private String encryptMethod(){
-        for(int i = 0; i <=userPhrase.length(); i++){
+
+
+
+        for(int i = 0; i < userPhrase.length(); i++){
             char currentChar = userPhrase.charAt(i);
             int ASCIIchar = (int) currentChar;
-            userPhraseArray[i] = ASCIIchar;
+            userPhraseEncrypted += (char)(ASCIIchar + (decryptKey % 10));
         }
 
-
+        return userPhraseEncrypted;
 
 
     }
+
+    public String getEncryptedPhrase(){
+
+        return "Your encrypted phrase is: " + encryptMethod() + "\nYour decrypt key for this message is: " + decryptKey;
+    }
+
+
+    private String decryptMethod(String userPhrase, int key){
+        String userPhraseDecrypted = "";
+
+        for(int i = 0; i < userPhrase.length(); i++){
+            char currentChar = userPhrase.charAt(i);
+            int ASCIIchar = (int) currentChar;
+            userPhraseDecrypted += (char)(ASCIIchar - (key % 10));
+        }
+
+        return userPhraseDecrypted;
+    }
+
+    public String getDecryptedPhrase(String userPhrase, int key){
+        return "Your decrypted phrase is: " + decryptMethod(userPhrase, key);
+    }
+
 
 }
